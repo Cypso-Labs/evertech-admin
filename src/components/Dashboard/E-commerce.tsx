0ea0048f65830bd66740f0ae00361b6d16bdb7f7
Mayuru Madhuranga <mayuru@cypsolabs.com>
2024-10-21 17:44:43 +0530

    develop Dashboard And services

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -7,21 +7,25 @@ import TableOne from "../Tables/TableOne";
 import MapOne from "../Maps/MapOne";
 import DataStatsOne from "@/components/DataStats/DataStatsOne";
 import ChartOne from "@/components/Charts/ChartOne";
+import Table from "../Table/Table";
 
 const ECommerce: React.FC = () => {
   return (
     <>
       <DataStatsOne />
-
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
+      <div className="mt-6">
+        <Table/>
+        </div>
+      <div className="mt-4 grid grid-cols-1 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5 ">
         <ChartOne />
-        <ChartTwo />
-        <ChartThree />
-        <MapOne />
+      {/*  <ChartTwo />
+        
+          <ChartThree />
+      <MapOne />
         <div className="col-span-12 xl:col-span-8">
           <TableOne />
         </div>
-        <ChatCard />
+        <ChatCard />*/}
       </div>
     </>
   );
