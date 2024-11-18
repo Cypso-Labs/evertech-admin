d687a7aa75384a32fea4662726577a5d344cf062
KavinduCypso <kavindu@cypsolabs.com>
2024-11-18 11:00:55 +0530

    fix notfound page build erro

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,12 +8,14 @@ const Custom404 = () => {
         <h1 className="text-6xl font-bold text-red-500">404</h1>
         <h2 className="mt-2 text-2xl text-gray-700">Page Not Found</h2>
         <p className="mt-4 text-gray-500">
-          Oops! The page you're looking for doesn't exist or has been moved.
+          Oops! The page you&apos;re looking for doesn&apos;t exist or has been
+          moved.
         </p>
-        <Link href="/" legacyBehavior>
-          <a className="mt-6 inline-block rounded-lg bg-blue-500 px-6 py-3 text-white transition hover:bg-blue-400">
-            Go Back to Homepage
-          </a>
+        <Link
+          href="/"
+          className="mt-6 inline-block rounded-lg bg-blue-500 px-6 py-3 text-white transition hover:bg-blue-400"
+        >
+          Go Back to Homepage
         </Link>
       </div>
     </div>
