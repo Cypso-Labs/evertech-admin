596f057d637389ce6f8dc0a78f2afd8784231660
dilshan78-lab <maneesha@cypsolabs.com>
2024-11-19 00:20:48 +0530

    change fetch url

--- src/app/redux/serviceSlice.tsx
+++ src/app/redux/serviceSlice.tsx
@@ -15,9 +15,7 @@ const initialState: getServicers = {
   }
 
   export const getService = createAsyncThunk("service/getService", async () => {
-    // const response = await fetch('http://localhost:5000/api/services');
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  
+    const response = await fetch('http://localhost:5000/api/services');
     return response.json();
   });
   
