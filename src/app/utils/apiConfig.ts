d252b046c5f818abd05fad7afcc446f2d6d26267
DulanjanaLakshan <dulanjana20013@gmail.com>
2025-05-13 13:12:22 +0530

    do some changes

--- src/app/utils/apiConfig.ts
+++ src/app/utils/apiConfig.ts
@@ -1,4 +1,4 @@
-//export const BASE_URL = "https://evertech.lk/api";
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://evertech.lk/api";
+// export const BASE_URL = "http://localhost:5000/api";
 export const TIME_OUT = 5000;
 
